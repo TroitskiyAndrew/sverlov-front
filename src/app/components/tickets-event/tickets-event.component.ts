48205227ac9f83adead6a29905b4e6628c85a214
AndreyTroitskiy <TroitskiyA@yandex.ru>
2026-02-25 20:20:47 +0700

    feat: event type

--- src/app/components/tickets-event/tickets-event.component.ts
+++ src/app/components/tickets-event/tickets-event.component.ts
@@ -1,7 +1,7 @@
 import { Component, computed, effect, signal } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { StateService } from '../../services/state.service';
-import { TICKET_NAMES } from '../../constants/constants';
+import { TICKET_NAMES, EVENT_NAMES } from '../../constants/constants';
 import { CommonModule } from '@angular/common';
 import { ApiService } from '../../services/api.service';
 import { environment } from '../../../environments/environment';
@@ -26,6 +26,7 @@ export class TicketsEventComponent {
     }
     return tickets.filter((ticket: any) => ticket.type > 0)
   });
+  eventNames = EVENT_NAMES;
 
   sales = signal<any[]>([])
   totalSales = signal<any>({})
@@ -33,13 +34,7 @@ export class TicketsEventComponent {
     const event = this.event();
     return event.type === 0 ? 'https://www.instagram.com/reel/DStbYzaCPpW/' : 'https://www.instagram.com/reel/DStbYzaCPpW/';
   });
-  description = computed(() => {
-    const event = this.event();
-    return event.type === 0 ? ` СТЕНДАП-КОНЦЕРТ, где будет отточенный проверенный материал, который
-      работает во всех уголках земли` : `СТЕНДАП-НЕТВОРКИНГ - импровизационное шоу Дмитрия Сверлова, где он
-      знакомится со зрителями в зале и “на приколе и стёбе” помогает им найти друзей,
-      отношения, клиентов, решения проблем`;
-  });
+
   place = computed(() => {
     const event = this.event();
     if (!event) {
